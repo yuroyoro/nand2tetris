313a4bd72d187157947b3423b82ac66f2a7f1691
Tomohito Ozaki <ozaki@yuroyoro.com>
2020-05-13 17:58:59 +0900

    Chapter 04

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -6,9 +6,90 @@
 // Runs an infinite loop that listens to the keyboard input.
 // When a key is pressed (any key), the program blackens the screen,
 // i.e. writes "black" in every pixel;
-// the screen should remain fully black as long as the key is pressed. 
+// the screen should remain fully black as long as the key is pressed.
 // When no key is pressed, the program clears the screen, i.e. writes
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+// keypress = 0
+@keypress
+M=0
+
+// fill
+@fill
+M=0
+
+(LOOP)
+  // read key pressed
+  @KBD
+  D=M
+
+  // set keypress state
+  @SET_KEYPRESS_FILL
+  D;JGT
+
+  // unset keypress
+  @keypress
+  M=0
+
+  @SET_KEYPRESS_END
+  0;JMP
+
+(SET_KEYPRESS_FILL)
+  @0
+  D=!A
+  @keypress
+  M=D
+
+(SET_KEYPRESS_END)
+  // jump to LOOP if fill - keypress == 0
+  @fill
+  D=M
+  @keypress
+  D=D-M
+  @LOOP
+  D;JEQ
+
+  // fill = keypress
+  @keypress
+  D=M
+  @fill
+  M=D
+
+  // fill screen
+  // i = 0
+  @i
+  M=0
+
+  // i = i + 16384(0x4000)
+  @SCREEN
+  D=A
+  @i
+  M=D+M
+
+(SCREEN_LOOP)
+  // fill screen
+  @fill
+  D=M
+  @i
+  A=M
+  M=D
+
+  // i = i + 1
+  @i
+  M=M+1
+  D=M
+
+  // jump to SCREEN_LOOP if 24576 (0x6000) - i >= 0
+  @KBD
+  D=D-A
+
+  @SCREEN_LOOP
+  D;JLT
+
+  // infinite loop
+  @LOOP
+  0;JMP
+
--- 04/mult/mult.asm
+++ 04/mult/mult.asm
@@ -7,3 +7,38 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+
+  // i = 0
+  @i
+  M=0
+
+  // R2 = 0
+  @2
+  M=0
+
+(LOOP)
+  // GOTO END if i - R1 >= 0
+  @i
+  D=M
+  @1
+  D=D-M
+  @END
+  D;JGE
+
+  // R2 = R2 + R0
+  @0
+  D=M
+
+  @2
+  M=D+M
+
+  // i = i + 1
+  @i
+  M=M+1
+
+  @LOOP
+  0;JMP
+
+(END)
+  @END
+  0;JMP
